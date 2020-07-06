b099546234f22002f4888c97e1d5a2e87cd4f988
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-06 22:11:18 +0600

    useState StateTwo file-counter2 added

---
+++ src/components/01-use-state/StateTwo.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const StateTwo = () => {
+    const initCount = 0;
+    const [count, setCount] = useState(initCount)
+    const IncreaseFive = () => {
+        for (let i = 0; i < 5; i++) {
+            setCount(prevCount => prevCount + 1)
+            // prevCount is a function that has access in the old count
+        }
+    }
+    return (
+        <div>
+            <h2>Count : {count}</h2>
+            <button onClick={() => setCount(initCount)}>Reset</button>
+            <button onClick={() => setCount(count + 1)}>Increase</button>
+            <button onClick={() => setCount(count - 1)}> Decrease</button>
+            <button onClick={IncreaseFive}>Increament 5</button>
+        </div>
+    );
+};
+
+export default StateTwo;
