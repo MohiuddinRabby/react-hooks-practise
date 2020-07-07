76b0d08b308d08ecbbdbaa0721bbe4f811763241
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-07 17:14:06 +0600

    App js updated

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import StateOne from './components/01-use-state/StateOne';
+import StateTwo from './components/01-use-state/StateTwo';
+import StateThree from './components/01-use-state/StateThree';
+import StateFour from './components/01-use-state/StateFour';
+import StateFive from './components/01-use-state/StateFive';
 
 
 const App = () => {
   return (
     <div>
-      <h1>Hello Hooks</h1>
+      <h1>Hooks-practise</h1>
+      {/* <StateOne /> */}
+      {/* <StateTwo/> */}
+      {/* <StateThree /> */}
+      {/* <StateFour /> */}
+      {/* <StateFive /> */}
     </div>
   );
 }
