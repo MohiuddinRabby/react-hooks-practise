206c62b16040d15a2df3995505017fd92674dbc6
MohiuddinRabby <rabbymohiuddin@gmail.com>
2020-07-07 22:13:29 +0600

    EffectTwo file- useEffect mouse event

--- src/components/02-use-effect/EffectTwo.js
+++ src/components/02-use-effect/EffectTwo.js
@@ -1,9 +1,18 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const EffectTwo = () => {
     const [x, setX] = useState(0)
     const [y, setY] = useState(0)
-    
+    const logMouseMove = e => {
+        console.log('logmousemoved')
+       setX(e.clientX)
+       setY(e.clientY)
+    }
+    useEffect(() => {
+        console.log('useEffect added');
+        window.addEventListener('mousemove', logMouseMove)
+    },[])
+
     return (
         <div>
             <h1>X - {x} Y - {y}</h1>
